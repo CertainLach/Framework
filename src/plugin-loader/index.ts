c2a207f8f4c277c6a972ff1113b810b1c4aa5cda
CertainLach <iam@lach.pw>
2018-12-01 16:05:10 +0000

    Move back to old schema

--- src/plugin-loader/index.ts
+++
@@ -1,5 +0,0 @@
-import SoftPluginLoader from "./SoftPluginLoader";
-import WebpackPluginLoader from "./WebpackPluginLoader";
-import IPlugin from "./IPlugin";
-
-export {SoftPluginLoader,WebpackPluginLoader,IPlugin};
--- src/rocket/index.ts
+++
@@ -1,8 +0,0 @@
-import ServerMiddleware from './ServerMiddleware';
-import initClient from './initClient';
-import Rocket from './Rocket';
-import {Component} from 'react';
-
-export default Rocket;
-export {ServerMiddleware,initClient};
-export {Component};
