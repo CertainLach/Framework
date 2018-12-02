7488444c2670d58f19d047d6152c59fd608b98db
CertainLach <iam@lach.pw>
2018-12-02 02:12:08 +0000

    Fix useless keys warning, move simple (Without props) fragment creation to h()

--- packages/rocket/h.ts
+++ packages/rocket/h.ts
@@ -2,17 +2,25 @@ import {Attributes, ComponentClass, FunctionComponent, ReactElement, ReactNode} 
 import React from "react";
 
 type IH = {
+    (el:ReactNode[]):ReactElement<void>,
+    <P extends object>(el:FunctionComponent<P> | ComponentClass<P> | string):ReactElement<void>,
     <P extends object>(el:FunctionComponent<P> | ComponentClass<P> | string, props: Attributes & P):ReactElement<P>,
     <P extends object>(el:FunctionComponent<P> | ComponentClass<P> | string, children: ReactNode[]):ReactElement<P>,
     <P extends object>(el:FunctionComponent<P> | ComponentClass<P> | string, props: Attributes & P, children: ReactNode[]):ReactElement<P>
 };
 // TODO: Optimizations (use keys where applicable)
 const h:IH = ((...args:any[])=>{
-    if(args.length===2){
+    if(args.length===1){
+        if(args[0] instanceof Array){
+            return h(React.Fragment,null,args[0]);
+        }else{
+            return React.createElement(args[0]);
+        }
+    }else if(args.length===2){
         if(args[1] instanceof Array){
             if(args[1].length===1)
                 return React.createElement(args[0],null,args[1][0]);
-            return React.createElement(args[0],null,args[1]);
+            return React.createElement(args[0],null,...args[1]);
         }else{
             return React.createElement(args[0],args[1]);
         }
@@ -22,14 +30,7 @@ const h:IH = ((...args:any[])=>{
         return React.createElement(args[0],args[1],...args[2]);
     }
 }) as any;
-type IFrag = {
-    (children: ReactNode[]):ReactElement<void>,
-    (props: Attributes, children: ReactNode[]):ReactElement<void>
-}
-const frag:IFrag = ((...args:any[])=>{
-    if(args.length === 1)
-        return h(React.Fragment,null,args[0]);
-    else
-        return h(React.Fragment, args[0], args[1]);
-}) as any;
+const frag = (p:object,el:ReactNode[])=>{
+    return h(React.Fragment,null,el);
+};
 export {h,frag};
