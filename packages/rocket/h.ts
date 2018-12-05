0073b365945452b3859bbda075088956c1871283
CertainLach <iam@lach.pw>
2018-12-05 21:22:08 +0000

    Critical quickfix

--- packages/rocket/h.ts
+++ packages/rocket/h.ts
@@ -36,11 +36,13 @@ const h: IH = ((...args: any[]) => {
                 return React.createElement(args[0], null, args[1][0]);
             return React.createElement(args[0], null, ...args[1]);
         } else {
-            processProps(args[1]);
+            if(args[1]!==null)
+                processProps(args[1]);
             return React.createElement(args[0], args[1]);
         }
     } else {
-        processProps(args[1]);
+        if(args[1]!==null)
+            processProps(args[1]);
         if (args[2].length === 1)
             return React.createElement(args[0], args[1], args[2][0]);
         return React.createElement(args[0], args[1], ...args[2]);
