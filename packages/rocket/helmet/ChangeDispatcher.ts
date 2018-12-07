91cf4b3a28c574b2c7f9a252f7c1827c72428176
CertainLach <iam@lach.pw>
2018-12-07 08:47:45 +0000

    Fix ChangeDispatcher not being dispatched on server

--- packages/rocket/helmet/ChangeDispatcher.ts
+++ packages/rocket/helmet/ChangeDispatcher.ts
@@ -5,14 +5,13 @@ import HelmetStore from "./HelmetStore";
 
 export default (props:{data:HelmetDataInstance}):JSX.Element=>{
     const helmetStore = useStore(HelmetStore);
-    const [added,setAdded] = useState(false);
     useEffect(()=>{
-        helmetStore.addInstance(props.data);
-        helmetStore.forceUpdate();
         return ()=>{
             helmetStore.removeInstance(props.data);
             helmetStore.forceUpdate();
         }
     });
+    helmetStore.addInstance(props.data);
+    helmetStore.forceUpdate();
     return null;
 }
