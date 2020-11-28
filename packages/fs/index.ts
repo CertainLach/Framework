629a4a9a35426c4b52e5b48f36a013a8c730ccee
Yaroslav Bolyukin <iam@lach.pw>
2020-11-29 03:59:18 +0500

    refactor(fs): drop support for legacy features
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- packages/fs/index.ts
+++ packages/fs/index.ts
@@ -1,135 +1,29 @@
 import { asyncEach, collectCallbacks } from '@meteor-it/utils';
-import * as fsNative from 'fs';
-import { Stats } from 'fs';
+import { constants } from 'fs';
+import { access, copyFile, mkdir, readdir, stat } from 'fs/promises';
 import * as path from 'path';
-
-type FileHandle = fsNative.promises.FileHandle;
-/**
- * Returns true if path is a valid data url
- * @param path path
- */
-function isDataUrl(path: string): boolean {
-	return /^data:.+\/.+;base64,/.test(path.substr(0, 268))
-}
-
-export interface IParsedDataUrl {
-	mime: string,
-	data: Buffer
-}
-/**
- * Returns mime and data of dataurl
- * @param path Data url
- */
-function parseDataUrl(path: string): IParsedDataUrl {
-	return {
-		mime: path.slice(5, path.indexOf(';')),
-		data: Buffer.from(path.slice(path.indexOf(',') + 1), 'base64')
-	}
-}
-
-/**
- * Get all files in directory
- */
-export async function readDir(dir: string) {
-	return await fsNative.promises.readdir(dir);
-}
-/**
- * Read file or parse data url
- * @param file Path to file to read
- */
-export async function readFile(file: string): Promise<Buffer> {
-	if (isDataUrl(file))
-		return parseDataUrl(file).data;
-	return await fsNative.promises.readFile(file);
-}
-
-export async function stat(file: string): Promise<Stats> {
-	return await fsNative.promises.stat(file);
-}
-
-export async function open(file: string, mode: string, access: string): Promise<FileHandle> {
-	return await fsNative.promises.open(file, mode, access);
-}
-
-export async function read(fd: FileHandle, buffer: Buffer, offset: number, length: number, position: number) {
-	return await fsNative.promises.read(fd, buffer, offset, length, position);
-}
 
 type IWalkOptions = {
 	filter?: (name: string) => boolean;
 }
 
-export async function copy(from: string, to: string, options?: IWalkOptions) {
+export async function copyWalk(from: string, to: string, options?: IWalkOptions) {
 	from = path.resolve(from);
 	to = path.resolve(to);
 	if ((await isDirectory(from))) {
 		const dirStruct = (await walkDirStructArray(from)).map(e => e.replace(from, to));
 		try {
-			await asyncEach(dirStruct, dir => mkdir(dir, true));
+			await asyncEach(dirStruct, dir => mkdir(dir, { recursive: true }));
 		} catch (e) { }
 		try {
-			await asyncEach(await walkDirArray(from), f => options && options.filter && !options.filter(f) && Promise.resolve(true) as any as Promise<void> || copy(f, f.replace(from, to)));
+			await asyncEach(await walkDirArray(from), f => options && options.filter && !options.filter(f) && Promise.resolve(true) as any as Promise<void> || copyWalk(f, f.replace(from, to)));
 		} catch (e) {
 			if (e.code !== 'ENOENT') throw e;
 		}
 		return;
 	}
-	await fsNative.promises.copyFile(from, to);
+	await copyFile(from, to);
 }
-
-export async function mkdir(pathStr: string, recursive: boolean = false): Promise<void> {
-	pathStr = path.resolve(pathStr);
-	if (recursive) {
-		// TODO: there is recursive option in new versions of node.js,
-		// check for availability of them
-		try {
-			return await mkdir(pathStr);
-		} catch (e) {
-			if (e.code === 'ENOENT') {
-				await mkdir(path.dirname(pathStr), true);
-				return await mkdir(pathStr);
-			} else if (!(await exists(pathStr))) {
-				throw e;
-			} else {
-				// Success
-				return;
-			}
-		}
-	} else {
-		return await fsNative.promises.mkdir(pathStr);
-	}
-}
-
-async function unlinkDirectoryStructure(pathStr: string) {
-	pathStr = path.resolve(pathStr);
-	const dirs = await readDir(pathStr);
-	await asyncEach(dirs, async dir => {
-		const dirPath = path.resolve(pathStr, dir);
-		await unlinkDirectoryStructure(dirPath);
-		await unlink(dirPath);
-	});
-}
-export async function unlink(pathStr: string, recursive: boolean = false, options?: IWalkOptions) {
-	pathStr = path.resolve(pathStr);
-	if (recursive) {
-		// Empty all dirs
-		await walkDirArray(pathStr, options).then(files => Promise.all(files.map((n) => unlink(n))));
-		await unlinkDirectoryStructure(pathStr);
-	} else {
-		if (await isDirectory(pathStr)) {
-			return await fsNative.promises.rmdir(pathStr);
-		}
-		await fsNative.promises.unlink(pathStr);
-	}
-}
-
-/**
- * Write text to file
- */
-export async function writeFile(filename: string, text: string | Buffer) {
-	return await fsNative.promises.writeFile(filename, text);
-}
-
 
 export function walkDirArray(dir: string, options?: IWalkOptions): Promise<string[]> {
 	return collectCallbacks(collector => walkDir(dir, collector, options));
@@ -143,7 +37,7 @@ export function walkDirArray(dir: string, options?: IWalkOptions): Promise<strin
 export async function walkDir(dir: string, cb?: (file: string) => void, options?: IWalkOptions): Promise<void> {
 	if (!await exists(dir)) { throw new Error('no such file or directory: ' + dir); }
 	let dirList: string[] = [];
-	await asyncEach(await readDir(dir), async (file: string) => {
+	await asyncEach(await readdir(dir), async (file: string) => {
 		let pathStr = dir + path.sep + file;
 		if (await isFile(pathStr)) {
 			if (options?.filter && !options.filter(pathStr)) return;
@@ -160,10 +54,11 @@ export async function walkDir(dir: string, cb?: (file: string) => void, options?
 export function walkDirStructArray(dir: string, options?: IWalkOptions): Promise<string[]> {
 	return collectCallbacks(collector => walkDirStruct(dir, collector, options));
 }
+
 export async function walkDirStruct(dir: string, cb: (dir: string) => void, options?: IWalkOptions): Promise<void> {
 	if (!await exists(dir)) { throw new Error('no such file or directory: ' + dir); }
 	let dirList: string[] = [];
-	await asyncEach(await readDir(dir), async (file: string) => {
+	await asyncEach(await readdir(dir), async (file: string) => {
 		let pathStr = path.resolve(dir, file);
 		if (await isDirectory(pathStr)) {
 			dirList.push(pathStr);
@@ -182,7 +77,7 @@ export async function walkDirStruct(dir: string, cb: (dir: string) => void, opti
  */
 export async function exists(file: string): Promise<boolean> {
 	try {
-		let result = await fsNative.promises.access(file, fsNative.constants.F_OK);
+		let result = await access(file, constants.F_OK);
 		return result === undefined;
 	} catch (e) {
 		// Because only "err" field is returned if not exists
@@ -202,18 +97,4 @@ export async function isFile(path: string): Promise<boolean> {
  */
 export async function isDirectory(path: string): Promise<boolean> {
 	return (await stat(path)).isDirectory();
-}
-
-/**
- * Wrapper to fs function
- */
-export function getReadStream(path: string, options = {}) {
-	return fsNative.createReadStream(path, options);
-}
-
-/**
- * Wrapper to fs function
- */
-export function getWriteStream(path: string, options = {}) {
-	return fsNative.createWriteStream(path, options);
 }
