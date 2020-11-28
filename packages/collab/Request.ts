7c10ad4be517c572932ed6c3eb87c2f694e5aef6
Yaroslav Bolyukin <iam@lach.pw>
2020-11-29 03:57:57 +0500

    chore: extract collab to separate repo
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- packages/collab/Request.ts
+++
@@ -1,7 +0,0 @@
-import DoRequest from './requests/Do';
-import UndoRequest from './requests/Undo';
-import RedoRequest from './requests/Redo';
-
-type Request=DoRequest|RedoRequest|UndoRequest;
-
-export default Request;
